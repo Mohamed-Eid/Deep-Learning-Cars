0aa684fa15fef7ac07786ba768aaa247852ae9c0
Mohamed-Eid <medoeid50@gmail.com>
2017-08-16 21:03:22 +0200

    Add files via upload

---
+++ UnityProject/Assets/Scripts/GUI/UIStartMenuController.cs
@@ -0,0 +1,22 @@
+﻿/// Author: Moahmed Eid
+/// Date: 8 / 2017
+
+#region Includes
+using UnityEngine;
+using UnityEngine.UI;
+#endregion
+
+/// <summary>
+/// Class for controlling the various ui elements of the simulation
+/// </summary>
+public class UIStartMenuController : MonoBehaviour
+{
+    #region Members
+    #endregion
+
+    #region Constructors
+    #endregion
+
+    #region Methods
+    #endregion
+}
